e677af5bfeced422bcd2f82bc6aa1704ccc8c501
kentarou.hashigata <git config --global user.email taro.kirameki@techacademy.jp>
2021-04-19 05:16:49 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :new, :show, :edit, :destroy]
-  before_action :correct_user, only: [:new, :show,:edit,:destroy]
+  before_action :correct_user, only: [:show,:edit,:destroy]
   def index
     @tasks = current_user.tasks
   end
